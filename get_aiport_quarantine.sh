124a4184f55b42fb8d5aa8008f7f6eb64882e188
Calico Kater <yasuhiro95@gmail.com>
2020-07-21 11:59:22 +0900

    Initial Commit

---
+++ get_aiport_quarantine.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+url="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000121431_00086.html"
+curl -s $url | grep -n "（空港検疫）" | xmllint --html --xpath '//a/@href' - | tr ' ' '\n' | grep  -v ^$ | tail -r | sed "s/\"//g" | cut -d '=' -f2 > airport_quarantine.txt
+
+for url in `cat airport_quarantine.txt`; do
+
+   date=`curl -s $url |  grep 'm-boxInfo__date' | sed -e 's/<[^>]*>//g'`
+   date=`echo $date | tr -d '\r'`
+
+   msg=`curl -s $url | grep 'が報告されました' | sed -e 's/<[^>]*>//g'`
+   msg=`echo $msg | tr -d '\r'`
+   msg=`echo $msg | sed "s/&#160;//g" | sed "s/^　//g"`
+
+   echo "\"$date\"","\"$url\"","\"$msg\""
+
+done > airport_quarantine.csv
