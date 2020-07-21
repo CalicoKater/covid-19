b66777d6ca56568f7f0867295d6e1b2b4e733d3d
Calico Kater <yasuhiro95@gmail.com>
2020-07-22 07:34:16 +0900

    test

--- test.sh
+++ test.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 c=0
+
+echo '<table border="1" cellpadding="0" cellspacing="0" width="50%">' > acc.html
+
 for url in `cat airport_quarantine_link_list.txt`; do
 #url="https://www.mhlw.go.jp/stf/newpage_10812.html"
   c=$((++c))
   p_key=$c
-  if [ $c -le 5 ]; then 
-    continue  
+
+  
+  if [ $c -eq 4 ]; then
+    cat no4.html 
+    continue
+  fi
+  # 重複のため削除
+  if [ $c -eq 15 || $c -le 5 ]; then 
+    continue
   fi
   #patch ni suru?
   #if [ "$c" = "10" ]; then
@@ -16,7 +26,7 @@ for url in `cat airport_quarantine_link_list.txt`; do
   #  cat 20200411_desc.csv
   #  continue
   #fi
-  curl $url | xmllint --html --xpath '//table' - \
+  curl $url | xmllint --html --xpath '//table/tbody' - \
     |  sed 'y/０１２３４５６７８９（）/0123456789()/' | tr -d '\r'
   #curl $url | xmllint --html --xpath '//table' - | sed -e 's/<[^>]*>//g' | tr -d '\t' \
   #  |  sed 'y/０１２３４５６７８９（）/0123456789()/' | tr -d '\r' > tmp.txt
@@ -25,6 +35,8 @@ for url in `cat airport_quarantine_link_list.txt`; do
 
   #debug
   # if [ "$c" = "18" ]; then break ; fi
- done > acc.html
+ done >> acc.html
+
 #done > airport_quarantine_description.csv
 
+echo "</table>" >> acc.html
