7b65dd773537bf8e1ffa2759df1f781598428235
Calico Kater <yasuhiro95@gmail.com>
2020-07-23 13:03:37 +0900

    pcr_test_Commit

--- airport_pcr_test.sh
+++ airport_pcr_test.sh
@@ -2,7 +2,7 @@ curl -s https://www.mhlw.go.jp/content/current_situation.csv | grep "空港検�
   | gawk -v FPAT='([^,]+)|(\"[^\"]+\")' '{print $2 "n" $3}' | tr -d '\\,+"()' | tr 'n' ',' > current.tmp
 
 s1=`cat current.tmp | cut -d, -f 3-4`
-date=`cat airport_quarantine2.csv | tr -d '"' | awk -F',' -f kkk.awk | grep $s1 | cut -d, -f 1`
+date=`cat airport_quarantine2.csv | tr -d '"' | awk -F',' -f airport_pcr_test.awk | grep $s1 | cut -d, -f 1`
 
 s2=`cat current.tmp`
 
