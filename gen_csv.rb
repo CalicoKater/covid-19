77567738ece17dabfd0b130c20d3e9cfd274a3bd
Calico Kater <yasuhiro95@gmail.com>
2020-07-26 11:12:25 +0900

    desc_test_csv

--- gen_csv.rb
+++ gen_csv.rb
@@ -3,39 +3,47 @@ require 'openssl'
 require 'nokogiri'
 require 'csv'
 require 'wareki'
-#date="2020-03-25"
+#report_date="2020-03-25"
 # url = "https://www.mhlw.go.jp/stf/newpage_10445.html"
-url = "https://www.mhlw.go.jp/stf/newpage_10673.html" #重複事例
-doc = Nokogiri::HTML.parse(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
+# url = "https://www.mhlw.go.jp/stf/newpage_10673.html" #重複事例
 
+url = ARGV[0]
+#url = "https://www.mhlw.go.jp/stf/newpage_12599.html"
+# url = "https://www.mhlw.go.jp/stf/newpage_10352.html"
+# url = "https://www.mhlw.go.jp/stf/newpage_10812.html"
+doc = Nokogiri::HTML.parse(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
 wareki = doc.search(:time).text
-date = Date.parse(wareki)
+report_date = Date.parse(wareki)
+
+case url
+when "https://www.mhlw.go.jp/stf/newpage_09966.html"
+  doc = Nokogiri::HTML.parse(File.read("no_1.html"))
+when "https://www.mhlw.go.jp/stf/newpage_10190.html"
+  doc = Nokogiri::HTML.parse(File.read("no_2.html"))
+when "https://www.mhlw.go.jp/stf/newpage_10200.html"
+  doc = Nokogiri::HTML.parse(File.read("no_3.html"))
+when "https://www.mhlw.go.jp/stf/newpage_10205.html"
+  doc = Nokogiri::HTML.parse(File.read("no_4_5.html"))
+when "https://www.mhlw.go.jp/stf/newpage_10313.html"
+  doc = Nokogiri::HTML.parse(File.read("no_6_7.html"))
+when "https://www.mhlw.go.jp/stf/newpage_10812.html"
+  doc = Nokogiri::HTML.parse(File.read("no_89_117.html"))
+when "https://www.mhlw.go.jp/stf/newpage_12034.html"
+  doc = Nokogiri::HTML.parse(File.read("no_280_282.html"))
+end
+
+#report_date=Date.parse("2021-03-23")
 
 table = doc.search(:table).first
 csv = CSV.generate{ |csv|
   table.search(:tr).each_with_index do |tr, row|
     if ( row > 0 ) then
-      csv << tr.search("th, td").map{|tag|tag.text.gsub(/(\r|\t)/, "")}
+      csv << tr.search("th, td").map{|tag|tag.text.gsub(/(\r|\t|\u00A0)/, "")}
     end
     #print "," ,row
   end
   #puts csv.
 }
-#puts csv
-#csv.table.each do |row|
-#  puts row
-#end
-
-File.open('a.csv', 'wb'){|f| f.puts(csv)}
-#f= CSV.parse(csv,liberal_parsing: {double_quote_outside_quote: true})
-#ccc = CSV.open('a.csv','r', liberal_parsing: true)
-#ccc.each do |row|
-#  p row
-#end
-#tbl = CSV.table('a.csv')
-#CSV.table("a.csv", headers: false, liberal_parsing: true) do |row|
-#  puts row
-#end
 
 f=CSV.parse(csv, headers: false, liberal_parsing: {double_quote_outside_quote: true})
 
@@ -44,32 +52,49 @@ f.each_with_index  do |line, n|
   if ( line[0] == "61" ) then 
     del_flg = 1
   end
-  arrival_date = "2020-03-24"
-  print date ,","    #公表日
-  print line[0], "," #事例
-  print '"', line[1], '"', "," #対応
-  print line[2], "," #年齢
-  print line[3], "," #性別
-  print line[4], "," #居住地
-  print '"', line[5], '"', "," #行動歴
-  print '"', line[6], '"', "," #症状
-  print arrival_date , ","  # 到着日
-  print del_flg  # 削除フラグ
+
+  if ( line[1].gsub( /[^0-9]/, "") == "" ) then
+    arrival_date = ""
+  else
+    arrival_date = line[1].tr('（）／０-９','()/0-9')
+    year = report_date.year
+    month = arrival_date.split("(")[1].split("/")[0]
+    day = arrival_date.split("/")[1].split(")")[0]
+    #arrival_date = "2020-03-24"
+    arrival_date = Date.parse("#{year}/#{month}/#{day}")
+    if (arrival_date > report_date ) then
+      arrival_date = arrival_date << 12
+    end
+  end
+
+  print report_date ,","    #公表日
+  print line[0].tr('/０-９','/0-9').gsub(/\n/,"").strip, "," #事例
+  print '"', line[1].tr('（）／０-９','()/0-9').strip, '"', "," #対応
+  print '"', line[2].gsub(/\n/,"").tr('/０-９','/0-9').strip, '"', "," #年齢
+  print '"', line[3].gsub(/\n/,"").strip, '"', "," #性別
+  
+  case line[4]
+  when "茨城"
+    print '"', "茨城県", '"', "," #居住地
+  when "佐賀"
+    print '"', "佐賀県", '"', "," #居住地
+  else
+    print '"', line[4].gsub(/\n|　/,"").strip, '"', "," #居住地
+  end
+
+  if ( line[5].size < 10 ) then
+    print '"', line[5].gsub(/\n/,"").strip, '"', "," #行動歴
+  else
+    print '"', line[5], '"', "," #行動歴
+  end
+  if (line[6].size < 10) then
+    print '"', line[6].gsub(/\n/,"").strip, '"', "," #症状
+  else
+    print '"', line[6], '"', "," #症状
+  end
+  print arrival_date, ","  # 到着日
+  print del_flg, ","  # 削除フラグ
+  print url # url
   puts
 end
-#CSV.open("a.csv", headers: false, liberal_parsing: true) do|row|
- #   p row
-#end
-#puts csv
-#tbl = CSV.table(csv)
-
-#tbl = csv.to_csv
-#csv.to_csv.each do |row|
-#  puts row
-#end
-
-# CSV.foreach(f, :headers => true, :force_quotes => true, :encoding => Encoding::UTF_8
-
-#file_path = 'index_db.csv'
-#File.open(file_path, 'wb'){|f| f.puts(tbl.to_csv)}
 
--- gomi/_abc.rb
+++ gomi/_abc.rb
@@ -3,9 +3,10 @@ require 'openssl'
 require 'nokogiri'
 require 'csv'
 require 'wareki'
-
-d="2020/03/25"
-doc = Nokogiri::HTML.parse(open("https://www.mhlw.go.jp/stf/newpage_10445.html",:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
+url="https://www.mhlw.go.jp/stf/newpage_10445.html"
+url=ARGV[0]
+#puts ARGV[0]
+doc = Nokogiri::HTML.parse(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
 date=doc.search(:time).text
 #puts date
 #y=date.split("令和")[1].split("年")
@@ -14,7 +15,6 @@ date=doc.search(:time).text
 #puts m[0].to_i
 #d=date.split("月")[1].split("日")
 #puts d[0].to_i
-
-#require 'wareki'
 puts Date.parse(date)
+
 
