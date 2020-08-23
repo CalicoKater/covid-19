16a56cccc33dd23f8d901c703c0dcf46aa88bbc5
Calico Kater <yasuhiro95@gmail.com>
2020-08-23 20:51:40 +0900

    20200823

--- src/airport_quarantine.sh
+++ src/airport_quarantine.sh
@@ -3,6 +3,11 @@ url="https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000121431_00086.html"
 curl -s $url | grep "（空港検疫）" | sed 's/<\/div>//g' | xmllint --html --xpath '//a/@href' - \
   | tr ' ' '\n' | grep  -v ^$ | tail -r | cut -d '"' -f 2 > ./var/airport_quarantine_link_list.txt
 
+# delay 2020-08-22
+# echo https://www.mhlw.go.jp/stf/newpage_13123.html >> ./var/airport_quarantine_link_list.txt
+# delay 2020-08-23 
+# echo https://www.mhlw.go.jp/stf/newpage_13128.html >> ./var/airport_quarantine_link_list.txt
+
 for url in `cat ./var/airport_quarantine_link_list.txt`; do  
 
    date=`curl -s $url |  grep 'm-boxInfo__date' | sed -e 's/<[^>]*>//g'` 
