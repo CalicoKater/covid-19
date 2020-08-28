eb64a8df6755a00e4d5a9ab02d9b2521c7de4858
Calico Kater <yasuhiro95@gmail.com>
2020-08-28 21:39:27 +0900

    20200828

--- work/tokyo_death_of_patient.rb
+++ work/tokyo_death_of_patient.rb
@@ -1,13 +1,9 @@
-#
-#
 #(
 #for url in `cat death_rep_list.txt`; do 
 #  #echo $url
 #  ruby tokyo_death_of_patient.rb $url
 #done
 #) >tokyo_death_of_patient.csv
-
-
 require 'open-uri'
 require 'openssl'
 require 'nokogiri'
@@ -15,15 +11,10 @@ require 'csv'
 require 'wareki'
 require 'net/http'
 require 'uri'
-
-
-
-#url = "https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/04/index.html"
-url = "https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/04/30/15.html"
+#url = "https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/04/30/15.html"
 url = "https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/04/01/21.html"
 #url = ARGV[0]
 uri = URI.parse(url)
-
 response_get = Net::HTTP::get(uri)
 response_get.force_encoding("UTF-8")
 press_date = Array.new
@@ -32,13 +23,8 @@ response_get.lines.each do |f|
     press_date = Date.parse(f.gsub(/[^0-9]/,""))
   end
 end
-
-puts press_date
-
+#puts press_date
 doc = Nokogiri::HTML.parse(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
-
-
-
 table = doc.search(:table).first
 csv = CSV.generate{ |csv|
   table.search(:tr).each_with_index do |tr, row|
@@ -47,5 +33,16 @@ csv = CSV.generate{ |csv|
     end
   end
 }
-
-puts csv
+#puts csv
+f=CSV.parse(csv, headers: false, liberal_parsing: {double_quote_outside_quote: true})
+f.each_with_index  do |line, n|
+  #confirmed_date = Date.parse(line[4])
+  #death_date = Date.parse(line[5])
+  print press_date, "," #プレス日付
+  print line[0], "," #枝番
+  print line[1], "," #年代
+  print line[2], "," #性別
+  print line[3], "," #居住地
+  print line[4], "," #診断日
+  puts line[5] #死亡日
+end
