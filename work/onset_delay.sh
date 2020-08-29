9226bdc52161d7889752ac3c56bf2820ccd7883d
Calico Kater <yasuhiro95@gmail.com>
2020-08-29 21:39:16 +0900

    20200829

--- work/onset_delay.sh
+++ work/onset_delay.sh
@@ -89,6 +89,11 @@ url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/24db4d9c5d
 update_at=`curl -s $url | jq -r '.date'`; date=`date  -j -f "%Y/%m/%d %H:%M" "$update_at" +"%Y-%m-%d"`
 curl -s $url | jq -r '.data[]|[.developed_date, .count]|@csv' > onset_$date.txt
 
+#8/29
+url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/2115bdd07e28920441fca91b460cfcf158c6754d/data/positive_by_developed.json"
+update_at=`curl -s $url | jq -r '.date'`; date=`date  -j -f "%Y/%m/%d %H:%M" "$update_at" +"%Y-%m-%d"`
+curl -s $url | jq -r '.data[]|[.developed_date, .count]|@csv' > onset_$date.txt
+
 ( day=2020-08-12; join -t, -a 1 -a 2 onset_2020-08-12.txt onset_2020-08-11.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
   day=2020-08-13; join -t, -a 1 -a 2 onset_2020-08-13.txt onset_2020-08-12.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
   day=2020-08-14; join -t, -a 1 -a 2 onset_2020-08-14.txt onset_2020-08-13.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
@@ -106,4 +111,5 @@ curl -s $url | jq -r '.data[]|[.developed_date, .count]|@csv' > onset_$date.txt
   day=2020-08-26; join -t, -a 1 -a 2 onset_2020-08-26.txt onset_2020-08-25.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
   day=2020-08-27; join -t, -a 1 -a 2 onset_2020-08-27.txt onset_2020-08-26.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
   day=2020-08-28; join -t, -a 1 -a 2 onset_2020-08-28.txt onset_2020-08-27.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
+  day=2020-08-29; join -t, -a 1 -a 2 onset_2020-08-29.txt onset_2020-08-28.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
 ) > df.txt
--- work/tokyo_death_of_patient.rb
+++ work/tokyo_death_of_patient.rb
@@ -12,8 +12,8 @@ require 'wareki'
 require 'net/http'
 require 'uri'
 #url = "https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/04/30/15.html"
-url = "https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/04/01/21.html"
-#url = ARGV[0]
+#url = "https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/04/01/21.html"
+url = ARGV[0]
 uri = URI.parse(url)
 response_get = Net::HTTP::get(uri)
 response_get.force_encoding("UTF-8")
@@ -29,20 +29,16 @@ table = doc.search(:table).first
 csv = CSV.generate{ |csv|
   table.search(:tr).each_with_index do |tr, row|
     if ( row > 0 ) then
-      csv << tr.search("th, td").map{|tag|tag.text.gsub(/(\r|\t|\u00A0)/, "")}
+      csv << tr.search("th, td").map{|tag|tag.text}
     end
   end
 }
 #puts csv
-f=CSV.parse(csv, headers: false, liberal_parsing: {double_quote_outside_quote: true})
+f = CSV.parse(csv, headers: false, liberal_parsing: {double_quote_outside_quote: true})
 f.each_with_index  do |line, n|
-  #confirmed_date = Date.parse(line[4])
-  #death_date = Date.parse(line[5])
-  print press_date, "," #プレス日付
-  print line[0], "," #枝番
-  print line[1], "," #年代
-  print line[2], "," #性別
-  print line[3], "," #居住地
-  print line[4], "," #診断日
-  puts line[5] #死亡日
+  # ＊＊＊ 年末年始 要修正（プレス日付と比較して加減算）＊＊＊
+  confirm_date = Date.parse("２０２０年" + line[4])
+  death_date = Date.parse("２０２０年" +line[5])
+  #プレス日付, 枝番, 年代, 性別, 居住地, 診断日, 死亡日, ソース
+  puts [ press_date, line[0], line[1], line[2], line[3], confirm_date, death_date, url ].join(",")
 end
