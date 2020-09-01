3daac8099337be6f20aa8adda8358a11066e5a38
Calico Kater <yasuhiro95@gmail.com>
2020-09-02 03:00:50 +0900

    tokyo_update_20200901

--- work/onset_delay.sh
+++ work/onset_delay.sh
@@ -104,6 +104,11 @@ url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/535b09941f
 update_at=`curl -s $url | jq -r '.date'`; date=`date  -j -f "%Y/%m/%d %H:%M" "$update_at" +"%Y-%m-%d"`
 curl -s $url | jq -r '.data[]|[.developed_date, .count]|@csv' > onset_$date.txt
 
+#9/1
+url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/151e9f6c37d07de19603724b0422611d2e72d695/data/positive_by_developed.json"
+update_at=`curl -s $url | jq -r '.date'`; date=`date  -j -f "%Y/%m/%d %H:%M" "$update_at" +"%Y-%m-%d"`
+curl -s $url | jq -r '.data[]|[.developed_date, .count]|@csv' > onset_$date.txt
+
 ( day=2020-08-12; join -t, -a 1 -a 2 onset_2020-08-12.txt onset_2020-08-11.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
   day=2020-08-13; join -t, -a 1 -a 2 onset_2020-08-13.txt onset_2020-08-12.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
   day=2020-08-14; join -t, -a 1 -a 2 onset_2020-08-14.txt onset_2020-08-13.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
@@ -124,4 +129,5 @@ curl -s $url | jq -r '.data[]|[.developed_date, .count]|@csv' > onset_$date.txt
   day=2020-08-29; join -t, -a 1 -a 2 onset_2020-08-29.txt onset_2020-08-28.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
   day=2020-08-30; join -t, -a 1 -a 2 onset_2020-08-30.txt onset_2020-08-29.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
   day=2020-08-31; join -t, -a 1 -a 2 onset_2020-08-31.txt onset_2020-08-30.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
+  day=2020-09-01; join -t, -a 1 -a 2 onset_2020-09-01.txt onset_2020-08-31.txt  | awk -v p=$day -F, '$2-$3!=0{print p,$1, $2-$3}'
 ) > df.txt
--- work/tokyo_death_of_patient.sh
+++ work/tokyo_death_of_patient.sh
@@ -1,39 +1,47 @@
 #!/bin/bash
 
-for i in `seq -f %02g 2 8`
-do
-  url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/$i/index.html"
-  #echo $url
-  sleep 1
-  curl -s $url | xmllint --html --xpath '//a' - | tr ' ' '\n' \
-  | grep "新型コロナウイルスに関連した患者の死亡" \
-  | sed -e 's/<\/a><a//g' -e 's/href=//g' -e 's/\"//g' -e 's/>/, /g' | tail -r \
-  > "tokyo_death_of_patient_2020$i.txt"
-done
-(
-  for url in `cut -d, -f 1 tokyo_death_of_patient_20200{4..7}.txt`; do
-    url="https://www.metro.tokyo.lg.jp$url"
-    sleep 1
-    ruby tokyo_death_of_patient.rb $url
-    #echo $url
-  done
-) > tokyo_death_of_patient_04_07.csv
-
-
-#url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/08/index.html"
+#for i in `seq -f %02g 2 8`
+#do
+#  url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/$i/index.html"
+#  #echo $url
+#  sleep 1
 #  curl -s $url | xmllint --html --xpath '//a' - | tr ' ' '\n' \
 #  | grep "新型コロナウイルスに関連した患者の死亡" \
 #  | sed -e 's/<\/a><a//g' -e 's/href=//g' -e 's/\"//g' -e 's/>/, /g' | tail -r \
-#  > "tokyo_death_of_patient_202008.txt"
+#  > "tokyo_death_of_patient_2020$i.txt"
+#done
+
+#(
+#  for url in `cut -d, -f 1 tokyo_death_of_patient_20200{4..7}.txt`; do
+#    url="https://www.metro.tokyo.lg.jp$url"
+#    sleep 1
+#    ruby tokyo_death_of_patient.rb $url
+#    #echo $url
+#  done
+#) > tokyo_death_of_patient_04_07.csv
+
+#(
+#  for url in `cut -d, -f 1 tokyo_death_of_patient_202008.txt`; do
+#    sleep 1
+#    url="https://www.metro.tokyo.lg.jp$url"
+#    ruby tokyo_death_of_patient.rb $url
+#  done
+#) > tokyo_death_of_patient_08.csv
+
+url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/09/index.html"
+  curl -s $url | xmllint --html --xpath '//a' - | tr ' ' '\n' \
+  | grep "新型コロナウイルスに関連した患者の死亡" \
+  | sed -e 's/<\/a><a//g' -e 's/href=//g' -e 's/\"//g' -e 's/>/, /g' | tail -r \
+  > tokyo_death_of_patient_202009.txt
 
 (
-  for url in `cut -d, -f 1 tokyo_death_of_patient_202008.txt`; do
+  for url in `cut -d, -f 1 tokyo_death_of_patient_202009.txt`; do
     sleep 1
     url="https://www.metro.tokyo.lg.jp$url"
     ruby tokyo_death_of_patient.rb $url
   done
-) > tokyo_death_of_patient_08.csv
+) > tokyo_death_of_patient_09.csv
 
 #echo "公表日,枝番,年代,性別,居住地,発症日,診断日,死亡日,ソース" > tokyo_death_of_patient.csv
 cat tokyo_death_of_patient_02_03.csv tokyo_death_of_patient_04_07.csv > tokyo_death_of_patient.csv
-cat tokyo_death_of_patient_08.csv >> tokyo_death_of_patient.csv
+cat tokyo_death_of_patient_08.csv tokyo_death_of_patient_09.csv >> tokyo_death_of_patient.csv
