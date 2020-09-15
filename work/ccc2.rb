1dc87e2229898990a2941fc6d72b1a1330f8f954
Calico Kater <yasuhiro95@gmail.com>
2020-09-15 22:15:44 +0900

    tokyo_update_20200915

--- work/ccc2.rb
+++ work/ccc2.rb
@@ -3,18 +3,22 @@ require 'openssl'
 require 'nokogiri'
 require 'csv'
 
-url = "https://www.mhlw.go.jp/stf/newpage_12674.html"
-#doc = Nokogiri::HTML.parse(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
-html = File.open('newpage_12674.html')
+#url = "https://www.mhlw.go.jp/stf/newpage_12674.html"
+#url = "https://www.pref.akita.lg.jp/pages/archive/47957"
+#url = ""
+url = ARGV[0]
+
+doc = Nokogiri::HTML.parse(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
+#html = File.open('newpage_12674.html')
 
 #doc = Nokogiri::HTML.parse(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
-doc = Nokogiri.parse(html)
+#doc = Nokogiri.parse(html)
 table = doc.search(:table)
 csv = CSV.generate{ |csv|
   table.search(:tr).each_with_index do |tr, row|
-    if ( row > 0 ) then
+    #if ( row > 0 ) then
       csv << tr.search("th, td").map{|tag|tag.text.gsub(/(\r|\t|\u00A0)/, "")}
-    end
+    #end
   end
 }
 puts csv
