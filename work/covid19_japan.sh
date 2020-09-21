acb516f19d89c69191d53eb959fe757af813886b
Calico Kater <yasuhiro95@gmail.com>
2020-09-21 16:23:08 +0900

    chiba

--- work/covid19_japan.sh
+++ work/covid19_japan.sh
@@ -74,6 +74,7 @@ xlsx2csv -s 6 12_chiba.xlsx >> 12_chiba.csv
 xlsx2csv -s 7 12_chiba.xlsx >> 12_chiba.csv
 xlsx2csv -s 8 12_chiba.xlsx | sed 's/^,//' > 12_chiba2.csv
 xlsx2csv -s 9 12_chiba.xlsx >> 12_chiba2.csv
+xlsx2csv -s 10 12_chiba.xlsx >> 12_chiba2.csv
 
 #13 東京都
 url="https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients.csv"
