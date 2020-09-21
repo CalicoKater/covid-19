e7cd938708dac2c674e45a126942949643d92bc9
Calico Kater <yasuhiro95@gmail.com>
2020-09-21 18:50:44 +0900

    patients_update_20200921_1850

--- work/covid19_japan.sh
+++ work/covid19_japan.sh
@@ -181,16 +181,21 @@ xlsx2csv -s 2 27_osaka3.xlsx >> 27_osaka2.csv
 url="http://www.pref.osaka.lg.jp/hodo/attach/hodo-39388_5.xlsx"
 curl -s -o 27_osaka4.xlsx $url
 xlsx2csv -s 2 27_osaka4.xlsx >> 27_osaka2.csv
+# 9/21
+url="http://www.pref.osaka.lg.jp/hodo/attach/hodo-39389_5.xlsx"
+curl -s -o 27_osaka5.xlsx $url
+xlsx2csv -s 2 27_osaka5.xlsx >> 27_osaka2.csv
 
 #07/01-
-rm 27_osaka3.csv
-for url in `cut -f 2 osaka_link.txt`; do
-  curl -s -o 27_osaka5.xlsx $url
-  xlsx2csv -s 2 27_osaka5.xlsx | sed -e '/^$/d' >> 27_osaka3.csv 
-done
+#rm 27_osaka3.csv
+#for url in `cut -f 2 osaka_link.txt`; do
+#  curl -s -o 27_osaka.xlsx $url
+#  xlsx2csv -s 2 27_osaka.xlsx | sed -e '/^$/d' >> 27_osaka3.csv 
+#done
 
 #28 兵庫県
-url="https://web.pref.hyogo.lg.jp/kk03/documents/corona-kanjajokyou.xlsx"
+#url="https://web.pref.hyogo.lg.jp/kk03/documents/corona-kanjajokyou.xlsx"
+url="https://web.pref.hyogo.lg.jp/kk03/documents/corona_kanjyajyokyo.xlsx"
 curl -s -o 28_hyogo.xlsx $url
 xlsx2csv 28_hyogo.xlsx | sed 's/^,//' > 28_hyogo.csv
 
