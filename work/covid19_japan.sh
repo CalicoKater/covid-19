901795011b8a21e4869f69add7d9695f2959ab7b
Calico Kater <yasuhiro95@gmail.com>
2020-09-28 17:14:17 +0900

    saitama_csv

--- work/covid19_japan.sh
+++ work/covid19_japan.sh
@@ -56,7 +56,7 @@ url=`curl -s https://opendata.pref.saitama.lg.jp/data/dataset/covid19-jokyo | gr
 curl -s $url | iconv -f SJIS > 11_saitama.csv
 link=`curl -s "https://www.pref.saitama.lg.jp/a0701/covid19/jokyo.html" | xmllint --html --xpath '//*[@id="tmp_contents"]/p[3]/a' - | cut -d\" -f 2`
 curl -s -o 11_saitama.pdf "https://www.pref.saitama.lg.jp$link"
-pdftotext -layout 11_saitama.pdf - | awk '{print $1, $2, $3, $4, $5, $6}' > 11_saitama2.csv
+pdftotext -layout 11_saitama.pdf - | awk '{printf "%s,%s,%s,%s,%s,%s\n", $1, $2, $3, $4, $5, $6}' > 11_saitama2.csv
 
 #12 千葉県
 link=`curl -s https://www.pref.chiba.lg.jp/shippei/press/2019/ncov-index.html | xmllint --html --xpath '//*[@id="tmp_contents"]/ul[1]/li[1]/a' - | cut -d\" -f 2`
