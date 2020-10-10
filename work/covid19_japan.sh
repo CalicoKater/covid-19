c42351a6e730bdfa6121984e3bdc1f3315e1a3b2
Calico Kater <yasuhiro95@gmail.com>
2020-10-11 01:52:00 +0900

    patients_update_202010110130

--- work/covid19_japan.sh
+++ work/covid19_japan.sh
@@ -318,14 +318,18 @@ curl -s $url | iconv -f SJIS > 22_shizuoka.csv
 
 #23 愛知県
 # 前月分までのPDF
-link=`curl -s "https://www.pref.aichi.jp/site/covid19-aichi/" | xmllint --html --xpath '//*[@id="main_body"]/div[1]/p[4]//span/a/@href' - | cut -d\" -f 2`
+link=`curl -s https://www.pref.aichi.jp/site/covid19-aichi/ | xmllint --html --xpath '//*[contains( ./text(),"9月まで [PDFファイル／")]/@href' - | cut -d\" -f 2`
 url="https://www.pref.aichi.jp/$link"
 curl -s -o 23_aichi.pdf $url
 
 # 当月分のPDF
-link=`curl -s "https://www.pref.aichi.jp/site/covid19-aichi/" | xmllint --html --xpath '//*[@id="main_body"]//p[4]/a/@href' - | cut -d\" -f 2`
+link=`curl -s https://www.pref.aichi.jp/site/covid19-aichi/ | xmllint --html --xpath '//*[contains( ./text(),"10月 [PDFファイル／")]/@href' - | cut -d\" -f 2`
 url="https://www.pref.aichi.jp/$link"
 curl -s -o 23_aichi2.pdf $url
+
+open https://www.ilovepdf.com/ja/pdf_to_excel
+sleep 5
+open .
 
 #ruby pdf_to_csv.rb 23_aichi.pdf | sed -e 's/^\[//' -e 's/\]$//' > 23_aichi.csv
 #ruby pdf_to_csv.rb 23_aichi2.pdf | sed -e 's/^\[//' -e 's/\]$//' >> 23_aichi.csv
