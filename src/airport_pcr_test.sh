eb514a956b4a5426791bd8bcee5cb5cff9b04580
Calico Kater <yasuhiro95@gmail.com>
2020-10-10 20:28:36 +0900

    airport_pcr_test.sh&patient_upatate_20201010_2030

--- src/airport_pcr_test.sh
+++ src/airport_pcr_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #curl -s https://www.mhlw.go.jp/content/current_situation.csv | iconv -f SJIS | grep "空港検疫" \
 curl -s https://www.mhlw.go.jp/content/current_situation.csv | grep "空港検疫" \
-  | gawk -v FPAT='([^,]+)|(\"[^\"]+\")' '{print $2 "n" $3}' | tr -d '\\,+"() ' | tr 'n' ',' > ./var/current.tmp
+  | gawk -v FPAT='([^,]+)|(\"[^\"]+\")' '{print $2 "n" $3}' | tr -d '\\,+"() ' | tr 'n' ',' | sed -e 's/※7//g' > ./var/current.tmp
 
 s1=`cat ./var/current.tmp | cut -d, -f 3-4`
 date=`cat airport_quarantine2.csv | tr -d '"' | awk -F',' -f ./src/airport_pcr_test.awk | grep $s1 | cut -d, -f 1`
