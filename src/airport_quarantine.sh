9bc9ac15e568adae5f730a26d4f0df3b49567932
Calico Kater <yasuhiro95@gmail.com>
2020-11-28 22:33:55 +0900

    airport update

--- src/airport_quarantine.sh
+++ src/airport_quarantine.sh
@@ -13,6 +13,8 @@ curl -s $url | grep "（空港検疫）" | sed 's/<\/div>//g' | xmllint --html -
 # echo https://www.mhlw.go.jp/stf/newpage_13535.html >> ./var/airport_quarantine_link_list.txt
 # delay 2020-09-20
 # echo https://www.mhlw.go.jp/stf/newpage_13674.html >> ./var/airport_quarantine_link_list.txt
+# delay 2020-11-28
+# echo https://www.mhlw.go.jp/stf/newpage_15141.html >> ./var/airport_quarantine_link_list.txt
 
 for url in `cat ./var/airport_quarantine_link_list.txt`; do  
 
