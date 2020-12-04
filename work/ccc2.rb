13c7ca8aef4dde0da1862855c1a7e059e53ca6b9
Calico Kater <yasuhiro95@gmail.com>
2020-12-04 21:08:10 +0900

    add user-agent

--- work/ccc2.rb
+++ work/ccc2.rb
@@ -4,12 +4,14 @@ require 'open-uri'
 require 'openssl'
 require 'nokogiri'
 require 'csv'
+
 #url = "https://www.mhlw.go.jp/stf/newpage_12674.html"
 #url = "https://www.pref.akita.lg.jp/pages/archive/47957"
+user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36'
 url = ARGV[0]
 uri = URI.parse(url)
 #doc = Nokogiri::HTML.parse(open(uri,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
-doc = Nokogiri::HTML.parse(URI.open(uri,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
+doc = Nokogiri::HTML.parse(URI.open(uri,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE, "User-Agent" => user_agent).open)
 #html = File.open('newpage_12674.html')
 #doc = Nokogiri::HTML.parse(open(url,:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE).open)
 #doc = Nokogiri.parse(html)
