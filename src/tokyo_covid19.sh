df70b0cc4c6c056defaa63a520bd3eb17c501253
Calico Kater <yasuhiro95@gmail.com>
2021-02-17 19:46:07 +0900

    tokyo update

--- src/tokyo_covid19.sh
+++ src/tokyo_covid19.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/daily_positive_detail.json"
+#url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/daily_positive_detail.json"
+url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/master/data/daily_positive_detail.json"
 (
   echo "報告日,陽性者数,接触歴等判明者数,接触歴等不明者数"
   curl -s $url | jq -r '.data[]|[.diagnosed_date, .count, .reported_count, .missing_count]|@csv'
 ) > tokyo_daily_positive_detail.csv
 
-url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/positive_by_diagnosed.json"
+#url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/positive_by_diagnosed.json"
+url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/master/data/positive_by_diagnosed.json"
 (
   echo "確定日,陽性者数"
   curl -s $url | jq -r '.data[]|[.diagnosed_date, .count]|@csv'
 #) > tokyo_positive_by_diagnosed.csv
 ) > tokyo_positive_by_confirmed.csv
 
-url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/positive_rate.json"
+#url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/positive_rate.json"
+url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/master/data/positive_rate.json"
 (
   echo "検査結果の判明日,PCR検査陽性者数,抗原検査陽性者数,PCR検査陰性者数,抗原検査陰性者数"
   curl -s $url | jq -r '.data[]|[.diagnosed_date, .pcr_positive_count, .antigen_positive_count, .pcr_negative_count, .antigen_negative_count]|@csv'
 ) > tokyo_positive_rate.csv
 
-url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/positive_by_developed.json"
+#url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/positive_by_developed.json"
+url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/master/data/positive_by_developed.json"
 (
   echo "発症日,陽性者数"
   curl -s $url | jq -r '.data[]|[.developed_date, .count]|@csv'
 ) > tokyo_positive_by_onset.csv
 
-url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/positive_status.json"
+#url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/development/data/positive_status.json"
+url="https://raw.githubusercontent.com/tokyo-metropolitan-gov/covid19/master/data/positive_status.json"
 (
   echo "日付,入院患者数,重症者数"
   curl -s $url | jq -r '.data[]|[.date, .hospitalized, .severe_case]|@csv'
