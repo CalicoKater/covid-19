221d26eb67d18e62d35eed9d2e053ec7c6eba626
Calico Kater <yasuhiro95@gmail.com>
2021-03-04 20:10:43 +0900

    tokyo commit

--- src/tokyo_death_of_patient.sh
+++ src/tokyo_death_of_patient.sh
@@ -32,11 +32,13 @@
 #url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/11/index.html"
 #url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2020/12/index.html"
 #url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/01/index.html"
-url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/02/index.html"
+#url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/02/index.html"
+url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/03/index.html"
   curl -s $url | xmllint --html --xpath '//a' - | tr ' ' '\n' \
   | grep "新型コロナウイルスに関連した患者の死亡" \
   | sed -e 's/<\/a><a//g' -e 's/href=//g' -e 's/\"//g' -e 's/>/, /g' | tail -r \
-  > ./var/tokyo_death_of_patient_202102.txt
+  > ./var/tokyo_death_of_patient_202103.txt
+#  > ./var/tokyo_death_of_patient_202102.txt
 #  > ./var/tokyo_death_of_patient_202101.txt
 #  > ./var/tokyo_death_of_patient_202011.txt
 #  > tokyo_death_of_patient_202009.txt
@@ -46,12 +48,14 @@ url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/02/index.html"
 #  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202011.txt`; do
 #  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202012.txt`; do
 #  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202101.txt`; do
-  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202102.txt`; do
+#  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202102.txt`; do
+  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202103.txt`; do
      sleep 1
     url="https://www.metro.tokyo.lg.jp$url"
     ruby ./src/tokyo_death_of_patient.rb $url
   done
-) > ./var/tokyo_death_of_patient_2102.csv
+) > ./var/tokyo_death_of_patient_2103.csv
+#) > ./var/tokyo_death_of_patient_2102.csv
 #) > ./var/tokyo_death_of_patient_2101.csv
 #) > ./var/tokyo_death_of_patient_12.csv
 #) > ./var/tokyo_death_of_patient_11.csv
@@ -61,7 +65,7 @@ url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/02/index.html"
 cat ./var/tokyo_death_of_patient_02_03.csv ./var/tokyo_death_of_patient_04_07.csv > tokyo_death_of_patient.csv
 cat ./var/tokyo_death_of_patient_08.csv ./var/tokyo_death_of_patient_09.csv >> tokyo_death_of_patient.csv
 cat ./var/tokyo_death_of_patient_10.csv ./var/tokyo_death_of_patient_11.csv ./var/tokyo_death_of_patient_12.csv >> tokyo_death_of_patient.csv
-cat ./var/tokyo_death_of_patient_2101.csv ./var/tokyo_death_of_patient_2102.csv >> tokyo_death_of_patient.csv
+cat ./var/tokyo_death_of_patient_2101.csv ./var/tokyo_death_of_patient_2102.csv ./var/tokyo_death_of_patient_2103.csv >> tokyo_death_of_patient.csv
 
 url="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/shibou.html"
 ruby ./work/ccc2.rb $url | awk -F, -f ./src/tokyo_deaths_by_date_of_death.awk > tokyo_deaths_by_date_of_death.csv
