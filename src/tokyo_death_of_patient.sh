8f00cb35b1a1f61e7f75256b83b218caffc61253
Calico Kater <yasuhiro95@gmail.com>
2021-06-04 06:19:04 +0900

    tokyo update

--- src/tokyo_death_of_patient.sh
+++ src/tokyo_death_of_patient.sh
@@ -35,11 +35,13 @@
 #url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/02/index.html"
 #url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/03/index.html"
 #url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/04/index.html"
-url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/05/index.html"
+#url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/05/index.html"
+url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/06/index.html"
   curl -s $url | xmllint --html --xpath '//a' - | tr '<' '\n' \
   | grep -e "新型コロナウイルスに関連した患者の死亡" -e "新型コロナウイルス関連 患者の死亡" \
   | sed -e 's/<\/a><a//g' -e 's/a href=//g' -e 's/\"//g' -e 's/>/, /g' | tail -r \
-  > ./var/tokyo_death_of_patient_202105.txt
+  > ./var/tokyo_death_of_patient_202106.txt
+#  > ./var/tokyo_death_of_patient_202105.txt
 #  > ./var/tokyo_death_of_patient_202104.txt
 #  > ./var/tokyo_death_of_patient_202103.txt
 #  > ./var/tokyo_death_of_patient_202102.txt
@@ -55,12 +57,14 @@ url="https://www.metro.tokyo.lg.jp/tosei/hodohappyo/press/2021/05/index.html"
 #  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202102.txt`; do
 #  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202103.txt`; do
 #  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202104.txt`; do
-  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202105.txt`; do
+#  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202105.txt`; do
+  for url in `cut -d, -f 1 ./var/tokyo_death_of_patient_202106.txt`; do
      sleep 1
     url="https://www.metro.tokyo.lg.jp$url"
     ruby ./src/tokyo_death_of_patient.rb $url
   done
-) > ./var/tokyo_death_of_patient_2105.csv
+) > ./var/tokyo_death_of_patient_2106.csv
+#) > ./var/tokyo_death_of_patient_2105.csv
 #) > ./var/tokyo_death_of_patient_2104.csv
 #) > ./var/tokyo_death_of_patient_2103.csv
 #) > ./var/tokyo_death_of_patient_2102.csv
@@ -74,6 +78,6 @@ cat ./var/tokyo_death_of_patient_02_03.csv ./var/tokyo_death_of_patient_04_07.cs
 cat ./var/tokyo_death_of_patient_08.csv ./var/tokyo_death_of_patient_09.csv >> tokyo_death_of_patient.csv
 cat ./var/tokyo_death_of_patient_10.csv ./var/tokyo_death_of_patient_11.csv ./var/tokyo_death_of_patient_12.csv >> tokyo_death_of_patient.csv
 cat ./var/tokyo_death_of_patient_2101.csv ./var/tokyo_death_of_patient_2102.csv ./var/tokyo_death_of_patient_2103.csv >> tokyo_death_of_patient.csv
-cat ./var/tokyo_death_of_patient_2104.csv ./var/tokyo_death_of_patient_2105.csv >> tokyo_death_of_patient.csv
+cat ./var/tokyo_death_of_patient_2104.csv ./var/tokyo_death_of_patient_2105.csv ./var/tokyo_death_of_patient_2106.csv >> tokyo_death_of_patient.csv
 url="https://www.fukushihoken.metro.tokyo.lg.jp/iryo/kansen/shibou.html"
 ruby ./work/ccc2.rb $url | awk -F, -f ./src/tokyo_deaths_by_date_of_death.awk > tokyo_deaths_by_date_of_death.csv
